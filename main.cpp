908280d9ca525477b07b1bc638ca6a3b036984f0
Robert Dulca <118079241+RobertDulca@users.noreply.github.com>
2023-04-21 11:53:05 +0200

    insert function hinzugefügt

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+};
+
+Node* insert(Node* root, int key) {
+    if (root == nullptr) {
+        root = new Node;
+        root->key = key;
+        root->left = nullptr;
+        root->right = nullptr;
+    } else if (key < root->key) {
+        root->left = insert(root->left, key);
+    } else if (key > root->key) {
+        root->right = insert(root->right, key);
+    }
+    return root;
+}
+
+void traverse(Node* root) {
+    if (root != nullptr) {
+        traverse(root->left);
+        cout << root->key << " ";
+        traverse(root->right);
+    }
+}
+
+int main() {
+    Node* root = nullptr;
+    ifstream file("input.txt");
+    int key;
+    while (file >> key) {
+        root = insert(root, key);
+    }
+    file.close();
+    traverse(root);
     return 0;
 }
+
